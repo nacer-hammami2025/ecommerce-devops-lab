78d0856637e25fd8d51ef2419dd64dd04f5b26d4
mohamed nacer <mohamednacer.hammami@iteam-univ.tn>
2026-05-12 19:59:46 +0100

    Switch to local backend for Terraform state

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,5 @@
 terraform {
-  backend "s3" {
-    bucket         = "ansible-project-terraform-state-1778612108"
-    key            = "prod/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
+  backend "local" {
+    path = "terraform.tfstate"
   }
 }
