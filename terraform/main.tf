436266a89e441643fe104da26ad9233bd08dbdb6
mohamed nacer <mohamednacer.hammami@iteam-univ.tn>
2026-05-12 19:46:43 +0100

    Rename ALB security group to ecommerce-lb-sg-v2

--- terraform/main.tf
+++ terraform/main.tf
@@ -59,7 +59,7 @@ data "aws_subnet" "default_az2" {
 
 # ALB Security Group
 resource "aws_security_group" "alb" {
-  name = "ecommerce-lb-sg"
+  name = "ecommerce-lb-sg-v2"
   description = "Security group for ALB"
   vpc_id      = data.aws_vpc.default.id
 
